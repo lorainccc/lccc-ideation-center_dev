0668ada3a1e7e1b20a6630bb0746eeee35f55166
Ryan <ryan@kiwicreative.net>
2017-08-02 17:21:24 -0400

    Menus function file
    Moved all the wp_nav_menus to this file for organization. Created functions for each menu so that it can easily be called where ever the menu is to be placed. *NOTE - Did not remove menus, walkers and fallbacks from header, page template and function.php yet. Will remove once I start editing those files.

--- functions/menu.php
+++ functions/menu.php
@@ -1,10 +1,166 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
+<?php
 
-<body>
-</body>
-</html>
+// This theme uses wp_nav_menu() in one location.
+register_nav_menus( 
+	array(
+		'primary' => esc_html__( 'Primary', 'lorainccc_subsite' ),
+		'left-nav' => esc_html__( 'Left Nav', 'lorainccc_subsite' ),
+		'footer-quicklinks-nav' => esc_html__( 'Footer Quicklinks', 'lorainccc' ),
+		'footer-campus-location-nav' => esc_html__( 'Footer Campus Locations', 'lorainccc' ),
+		'mobile-primary' => esc_html__( 'Mobile Primary Menu', 'lorainccc_subsite' ),
+  		'header-shortcuts' => esc_html__( 'Header Shortcuts Menu', 'lorainccc' ),
+  		'mobile-header-shortcuts' => esc_html__( 'Mobile Header Shortcuts Menu', 'lorainccc' ),
+	) 
+);
+
+/***********************************************************
+Existing Menus, Walkers and Fallbacks Built into theme
+***********************************************************/
+
+// Primary Nav Menu
+function primary_nav() {
+	wp_nav_menu(array(
+		'container' => false,
+		'menu' => __( 'Primary', 'textdomain' ),
+		'menu_class' => 'dropdown menu hide-for-print',
+		'theme_location' => 'primary',
+		'items_wrap'      => '<ul id="%1$s" class="%2$s" data-dropdown-menu>%3$s</ul>',
+		//Recommend setting this to false, but if you need a fallback...
+		'fallback_cb' => 'lc_topbar_menu_fallback',
+		'walker' => new lc_top_bar_menu_walker,
+	));
+}
+
+// Walker for Primary Nav Menu
+class lc_top_bar_menu_walker extends Walker_Nav_Menu {
+	//Add vertical menu class and submenu data attribute to sub menus
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\" data-submenu>\n";
+	}
+}
+
+// Fallback for Primary Nav Menu
+function lc_topbar_menu_fallback($args) {
+	/*
+	 * Instantiate new Page Walker class instead of applying a filter to the
+	 * "wp_page_menu" function in the event there are multiple active menus in theme.
+	*/
+
+	$walker_page = new Walker_Page();
+	$fallback = $walker_page->walk(get_pages(), 0);
+	$fallback = str_replace("<ul class='children'>", '<ul class="children submenu menu vertical" data-submenu>', $fallback);
+
+	echo '<ul class="dropdown menu" data-dropdown-menu">'.$fallback.'</ul>';
+}
+
+// Primary Menu on Mobile
+function primary_nav_mobile() {
+	wp_nav_menu(array(
+		'container' => false,
+		'menu' => __( 'Drill Menu', 'textdomain' ),
+		'menu_class' => 'vertical menu',
+		'theme_location' => 'mobile-primary',
+		'menu_id' => 'mobile-primary-menu',
+		//Recommend setting this to false, but if you need a fallback...
+		'fallback_cb' => 'lc_drill_menu_fallback',
+		'walker' => new lc_drill_menu_walker(),
+	));
+}
+
+// Walker for Primary Menu on Mobile
+class lc_drill_menu_walker extends Walker_Nav_Menu {
+	// Add vertical menu class
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\">\n";
+	}
+}
+
+// Fallback for Primary Menu on Mobile
+function lc_drill_menu_fallback($args) {
+	/*
+	 * Instantiate new Page Walker class instead of applying a filter to the
+	 * "wp_page_menu" function in the event there are multiple active menus in theme.
+	*/
+
+	$walker_page = new Walker_Page();
+	$fallback = $walker_page->walk(get_pages(), 0);
+	$fallback = str_replace("children", "children vertical menu", $fallback);
+	echo '<ul class="vertical menu" data-drilldown="">'.$fallback.'</ul>';
+}
+
+// Header Shortcuts Menu
+function header_shortcuts_menu() {
+	wp_nav_menu(array(
+		'container' => false,
+		'menu' => __( 'Header Shortcuts Menu', 'textdomain' ),
+		'menu_class' => 'menu align-right hide-for-print',
+		'theme_location' => 'header-shortcuts',
+		'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	));
+}
+
+
+// Header Shortcuts Menu for Mobile
+function header_shortcuts_mobile_menu() {
+	wp_nav_menu(array(
+		'container' => false,
+		'menu' => __( 'Drill Menu', 'textdomain' ),
+		'menu_class' => 'vertical menu',
+		'theme_location' => 'mobile-header-shortcuts',
+		'menu_id' => 'mobile-header-shortcuts',
+		//Recommend setting this to false, but if you need a fallback...
+		'fallback_cb' => 'lc_drill_menu_fallback',
+		'walker' => new lc_drill_menu_walker(),
+	));
+}
+
+// Drilldown Submenu for Mobile
+function mobile_submenu() {
+	wp_nav_menu(array(
+    	'container' => false,
+        'menu' => __( 'Drill Menu', 'textdomain' ),
+        'menu_class' => 'vertical menu',
+        'theme_location' => 'left-nav',
+        'menu_id' => 'sub-mobile-primary-menu',
+        //Recommend setting this to false, but if you need a fallback...
+        'fallback_cb' => 'lc_drill_menu_fallback',
+        'walker' => new lc_drill_menu_walker(),
+	));
+}
+
+
+// Left Sidebar Nav Menu
+function left_nav() {
+	// Primary navigation menu.
+	wp_nav_menu( array(
+		'menu_class'     => 'nav-menu',
+		'theme_location' => 'left-nav',
+	));
+}
+
+// Footer Campus Locations Nav
+function footer_campus_locations_nav() {
+	wp_nav_menu( array(
+		'menu_class'     => 'underline',
+		'theme_location' => 'footer-campus-location-nav',
+	));
+}
+
+// Primary Footer Nav
+function footer_quicklinks_nav() {
+	wp_nav_menu( array(
+		'menu_class'     => 'underline',
+		'theme_location' => 'footer-quicklinks-nav',
+	));
+}
+
+/**********************************************************************
+Menus, Walkers and Fallbacks added by Kiwi Creative for Campana theme
+***********************************************************************/
+
+// Campana Primary Nav
+function campana_primary_nav() {
+	// To Be Added
+}
