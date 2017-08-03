7f5c369932c120e735bf83dd5fc9137d5696ce47
Ryan <ryan@kiwicreative.net>
2017-08-03 16:31:08 -0400

    Created Nav Top Bar template part
    File for sticky nav

---
+++ template-parts/nav-topbar.php
@@ -0,0 +1,17 @@
+<div data-sticky-container>
+	<div data-sticky data-margin-top='0' data-top-anchor="masthead">
+		<div class="top-bar topbar-sticky-shrink">
+			<div class="top-bar-title">
+				<!-- Logo Here -->
+			</div>
+			<div class="top-bar-right">
+				<div class="top-nav-container">
+					<?php campana_top_nav(); ?>
+				</div>
+				<div class="main-nav-container">
+					<?php campana_main_nav(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
