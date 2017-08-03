a5973daba874a9d0bb4fd83276402aafb3bfccbf
Ryan <ryan@kiwicreative.net>
2017-08-03 15:18:16 -0400

    Stripping header down
    Removed all HTML specific to the LCCC site. Will be building header from scratch. Kept some of the class names on the page wrapper div, header tag and site-content div.

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
  *
  * @package LCCC Framework
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -28,121 +29,11 @@
 </script>
 </head>
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text hide-for-print" href="#content"><?php esc_html_e( 'Skip to content', 'lccc-framework' ); ?></a>
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text hide-for-print" href="#content"><?php esc_html_e( 'Skip to content', 'lccc-framework' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		  <div class="row show-for-medium">
-    <div class="large-6 medium-6 columns"><a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/LCCC-Logo.png" height="70" width="325" alt="Lorain County Community College Logo" /></a>  </div>
-    <div class="large-6 medium-6 columns">
-     									<?php
-          wp_nav_menu(array(
-											'container' => false,
-											'menu' => __( 'Header Shortcuts Menu', 'textdomain' ),
-											'menu_class' => 'menu align-right hide-for-print',
-											'theme_location' => 'header-shortcuts',
-											'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-												));
-											?>
-      <!-- This should be similar to what is generated when using Wordpress searchform.php -->
-					   <div class="large-9 medium-6 columns searchbox hide-for-print">
-          <?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { ?>
-																<?php dynamic_sidebar( 'lccc-search-sidebar' ); ?>
-								<?php }else{
-															if ( class_exists( 'CustomGoogleSearch' ) ) {
-																// check for plugin using plugin name
-																// Plugin is activated
-																		if ( is_active_sidebar( 'lccc-four-o-four-sidebar' ) ) {
+		<header id="masthead" class="site-header" role="banner">
+		  
+		</header><!-- #masthead -->
 
-																		}else{
-																			$instance = array(
-																			'display_results' => '3',
-																		);                                               the_widget('CGS_Widget', $instance);
-																			}
-															}else{
-																echo 'no widgets found';
-															}
-								}		?>
-					</div>
-    </div>
-  </div>
-<div class="medium-blue-bg show-for-medium">
-    <div class="row">
-      <div class="large-12 columns">
-        <nav class="menu-centered">
-									<?php
-          wp_nav_menu(array(
-											'container' => false,
-											'menu' => __( 'Primary', 'textdomain' ),
-											'menu_class' => 'dropdown menu hide-for-print',
-											'theme_location' => 'primary',
-											'items_wrap'      => '<ul id="%1$s" class="%2$s" data-dropdown-menu>%3$s</ul>',
-											//Recommend setting this to false, but if you need a fallback...
-											'fallback_cb' => 'lc_topbar_menu_fallback',
-											'walker' => new lc_top_bar_menu_walker,
-												));
-											?>
-        </nav>
-      </div>
-    </div>
-  </div>
-  <div class="row show-for-small-only mobile-nav-bar hide-for-print">
-    <div class="small-8 columns"> <a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="165" height="31.875" /></a> </div>
-    <div class="small-2 columns clearfix"> <span data-responsive-toggle="mobile-search" data-hide-for="medium"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/magnifying-glass.svg" height="25" width="25" alt="" class="float-right" data-toggle/></span> </div>
-    <div class="small-2 columns"> <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-      <button class="menu-icon" type="button" data-toggle></button>
-      </span> </div>
-  </div>
-  <div id="mobile-search" class="show-for-small-only hide-for-print">
-         			<?php if ( is_active_sidebar( 'lccc-search-sidebar' ) ) { ?>
-																<?php dynamic_sidebar( 'lccc-search-sidebar' ); ?>
-								<?php }else{
-															if ( class_exists( 'CustomGoogleSearch' ) ) {
-																// check for plugin using plugin name
-																// Plugin is activated
-																		if ( is_active_sidebar( 'lccc-four-o-four-sidebar' ) ) {
-																			echo 'sidebar active and plugin active';
-																		}else{
-																			$instance = array(
-																			'display_results' => '3',
-																		);                                               the_widget('CGS_Widget', $instance);
-																			}
-															}else{
-																echo 'no widgets found';
-															}
-								}		?>
-  </div>
-  <div id="responsive-menu" class="show-for-small-only hide-for-print">
-    <ul class="vertical menu" data-drilldown data-parent-link="true">
-     <li><a href="/" alt="Link back to LCCC Home Page">Home</a></li>
-					<?php 	wp_nav_menu(array(
-													'container' => false,
-													'menu' => __( 'Drill Menu', 'textdomain' ),
-													'menu_class' => 'vertical menu',
-													'theme_location' => 'mobile-primary',
-													'menu_id' => 'mobile-primary-menu',
-														//Recommend setting this to false, but if you need a fallback...
-													'fallback_cb' => 'lc_drill_menu_fallback',
-													'walker' => new lc_drill_menu_walker(),
-												));
-     ?>
-     <li>&nbsp;</li>
-     <?php
-            wp_nav_menu(array(
-													'container' => false,
-													'menu' => __( 'Drill Menu', 'textdomain' ),
-													'menu_class' => 'vertical menu',
-													'theme_location' => 'mobile-header-shortcuts',
-													'menu_id' => 'mobile-header-shortcuts',
-														//Recommend setting this to false, but if you need a fallback...
-													'fallback_cb' => 'lc_drill_menu_fallback',
-													'walker' => new lc_drill_menu_walker(),
-												));
-					?>
-    </ul>
-  </div>
-
-		</div>
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
