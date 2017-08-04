0da5b5370b762f9315b9a7647708b4c36380a9b8
Ryan <ryan@kiwicreative.net>
2017-08-04 17:23:23 -0400

    Added test class for testing
    Added test class to see if would show up on the dev site

--- template-parts/nav-topbar.php
+++ template-parts/nav-topbar.php
@@ -1,6 +1,6 @@
 <div class="column row">
 	<div data-sticky-container>
-		<div data-sticky data-margin-top='0' data-top-anchor="masthead" style="width: 100%;">
+		<div class="test" data-sticky data-margin-top='0' data-top-anchor="masthead" style="width: 100%;">
 			<div class="top-bar topbar-sticky-shrink">
 				<div class="top-bar-title">
 					<a href="<?php echo home_url(); ?>" title="Link to Homepage"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/LCCC-Campana-logo.svg" alt="Campana Center for Ideation and Invention of Lorain County Community College" height="71" width="282" /></a>
