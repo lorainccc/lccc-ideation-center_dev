77d1a0ee854ac7ddeea6de9cf626b3dab278f101
Ryan <ryan@kiwicreative.net>
2017-08-04 15:16:19 -0400

    Fixing Image link to Parent theme
    Changed image path to use get_stylesheet_directory_uri()

--- template-parts/nav-topbar.php
+++ template-parts/nav-topbar.php
@@ -1,9 +1,9 @@
-<div data-sticky-container>
-	<div data-sticky data-margin-top='0' data-top-anchor="masthead">
-		<div class="column row">
+<div class="column row">
+	<div data-sticky-container>
+		<div data-sticky data-margin-top='0' data-top-anchor="masthead">
 			<div class="top-bar topbar-sticky-shrink">
 				<div class="top-bar-title">
-					<a href="<?php echo home_url(); ?>" title="Link to Homepage"><img src="<?php echo get_template_directory_uri(); ?>/images/LCCC-Campana-logo.svg" alt="Campana Center for Ideation and Invention of Lorain County Community College" height="71" width="282" /></a>
+					<a href="<?php echo home_url(); ?>" title="Link to Homepage"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/LCCC-Campana-logo.svg" alt="Campana Center for Ideation and Invention of Lorain County Community College" height="71" width="282" /></a>
 				</div>
 				<div class="top-bar-right show-for-medium">
 					<div class="top-nav-container">
