eb811122e4581260c4cc038dc68570df79ab3e90
Ryan <ryan@kiwicreative.net>
2017-08-04 17:53:54 -0400

    changed text alignment to float

--- template-parts/nav-topbar.php
+++ template-parts/nav-topbar.php
@@ -6,10 +6,10 @@
 					<a href="<?php echo home_url(); ?>" title="Link to Homepage"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/LCCC-Campana-logo.svg" alt="Campana Center for Ideation and Invention of Lorain County Community College" height="71" width="282" /></a>
 				</div>
 				<div class="top-bar-right show-for-medium">
-					<div class="top-nav-container text-right">
+					<div class="top-nav-container float-right">
 						<?php campana_top_nav(); ?>
 					</div>
-					<div class="main-nav-container text-right">
+					<div class="main-nav-container float-right">
 						<?php campana_main_nav(); ?>
 					</div>
 				</div>
