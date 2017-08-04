8612517f4305be772a0600fd7d34d59df45aec2b
Ryan <ryan@kiwicreative.net>
2017-08-04 09:59:20 -0400

    Added Logo
    Added logo

--- template-parts/nav-topbar.php
+++ template-parts/nav-topbar.php
@@ -2,7 +2,7 @@
 	<div data-sticky data-margin-top='0' data-top-anchor="masthead">
 		<div class="top-bar topbar-sticky-shrink">
 			<div class="top-bar-title">
-				<!-- Logo Here -->
+				<a href="<?php echo home_url(); ?>" title="Link to Homepage"><img src="<?php echo get_template_directory_uri(); ?>/images/LCCC-Campana-logo.svg" alt="Campana Center for Ideation and Invention of Lorain County Community College" height="71" width="282" /></a>
 			</div>
 			<div class="top-bar-right">
 				<div class="top-nav-container">
