fd464b95f5138c472dd00dd9abce9aeffd8c9e9f
Ryan <ryan@kiwicreative.net>
2017-08-29 18:16:55 -0400

    Created blank contact us template

---
+++ templates/template-contact.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Contact
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<div id="full-width-content">
+	
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+	<main id="main" role="main">
+	
+		<?php 
+		
+		get_template_part( 'template-parts/content', 'banner' );
+		
+		get_template_part( 'template-parts/content', 'standard-intro');
+		
+		?>
+	
+	</main> <!-- end #main -->
+	
+	<?php endwhile; endif; ?>
+
+</div> <!-- end #full-width-content -->
+
+<?php get_footer(); ?>
