1dc3f1d15ac81b9cd242ef3922f8126d0ab86dd9
Ryan <ryan@kiwicreative.net>
2017-08-31 17:10:28 -0400

    tyring different pagination approach

--- archive-lccc_events.php
+++ archive-lccc_events.php
@@ -48,8 +48,12 @@
 			
 				endwhile;
 			
-			campana_page_navi();
-			
+				the_posts_pagination( array(
+					'mid_size'  => 2,
+					'prev_text' => 'Previous',
+					'next_text' => 'Next',
+				) );
+						
 			endif;
 			
 			?>
