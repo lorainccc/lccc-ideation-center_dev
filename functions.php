70afa79d72b79d5cca9e1ea7a08d25dfff3b3584
Ryan <ryan@kiwicreative.net>
2017-10-09 14:27:28 -0400

    tabindex

--- functions.php
+++ functions.php
@@ -354,7 +354,7 @@ function offcanvas_tab_index() {
 
 <script>
 	
-	jQuery("#offCanvas a, #offCanva button, #offCanvas #gsc-i-id1, #offCanvas input").attr("tabindex", 1);
+	jQuery("#offCanvas ul > li > a, #offCanva button.menu-icon, #offCanvas #gsc-i-id1, #offCanvas input").attr("tabindex", 1);
 	
 </script>
 
